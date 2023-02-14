1092fecdf9238dd5046e980b28ffc7ad62dc7e65
Bryce York <bryce@bryceyork.com>
2023-02-13 16:36:45 -0800

    feat: better font

--- app/root.tsx
+++ app/root.tsx
@@ -6,13 +6,13 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import stylesUrl from "./styles/styles.css";
-import agGridCSS from 'ag-grid-community/styles/ag-grid.css';
-import agGridThemeCSS from 'ag-grid-community/styles/ag-theme-alpine.css';
+import agGridCSS from "ag-grid-community/styles/ag-grid.css";
+import agGridThemeCSS from "ag-grid-community/styles/ag-theme-alpine.css";
 
 import { getUser } from "./session.server";
 
@@ -20,35 +20,36 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl },
           { rel: "stylesheet", href: stylesUrl },
           { rel: "stylesheet", href: agGridCSS },
-          { rel: "stylesheet", href: agGridThemeCSS }
-        ];
+          { rel: "stylesheet", href: agGridThemeCSS },
+          { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" }
+  ];
 };
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
+  viewport: "width=device-width,initial-scale=1"
 });
 
 export async function loader({ request }: LoaderArgs) {
   return json({
-    user: await getUser(request),
+    user: await getUser(request)
   });
 }
 
 export default function App() {
   return (
     <html lang="en" className="h-full">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+    <head>
+      <Meta />
+      <Links />
+    </head>
+    <body className="h-full">
+    <Outlet />
+    <ScrollRestoration />
+    <Scripts />
+    <LiveReload />
+    </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}", './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ["./app/**/*.{ts,tsx,jsx,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [
-    require('tw-elements/dist/plugin')
-  ],
+    require("tw-elements/dist/plugin")
+  ]
 };
