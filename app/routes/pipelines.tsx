309fe76922139c5ab5a1140593c67bc0b9052908
Bryce York <bryce@bryceyork.com>
2023-02-13 18:56:04 -0800

    feat: better empty state for pipelines (and other polish)

--- app/routes/pipelines.tsx
+++ app/routes/pipelines.tsx
@@ -1,15 +1,21 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
-import { ChevronRightIcon, ChevronDownIcon, Square3Stack3DIcon as PipelineIcon, PlusIcon } from '@heroicons/react/24/outline';
+import {
+  ChevronRightIcon,
+  ChevronDownIcon,
+  Square3Stack3DIcon as PipelineIcon,
+  PlusIcon
+} from "@heroicons/react/24/outline";
 
 
 import { requireUserId } from "~/session.server";
-import { useUser, /*mydebug*/ } from "~/utils";
+import { useUser /*mydebug*/ } from "~/utils";
 import { getPipelineListItems } from "~/models/pipeline.server";
+import * as React from "react";
 
 if (typeof document != "undefined") {
-  import("tw-elements" as any)
+  import("tw-elements" as any);
 }
 
 export async function loader({ request }: LoaderArgs) {
@@ -32,7 +38,7 @@ export default function PipelinesPage() {
           <Link to=".">Pipelines</Link>
         </h1>
         <div className="flex space-x-6">
-          <button className='text-sm' disabled>{user.email}</button>
+          <button className="text-sm" disabled>{user.email}</button>
           <Form action="/logout" method="post">
             <button
               type="submit"
@@ -66,25 +72,26 @@ export default function PipelinesPage() {
         <div className="w-80 border-r bg-gray-50">
         </div>
         <div className="flex px-6 space-x-1 md:space-x-3">
-          <div className="dropdown relative">
-            <button
-              className="dropdown-toggle px-4 py-2.5 bg-blue-600 text-white font-medium text-sm leading-tight
+          {data.pipelineListItems.length !== 0 && (
+            <div className="dropdown relative">
+              <button
+                className="dropdown-toggle px-4 py-2.5 bg-blue-600 text-white font-medium text-sm leading-tight
                   rounded shadow-sm hover:bg-blue-700 hover:shadow-md focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
                   active:bg-blue-800 active:shadow-lg active:text-white
                   transition duration-150 ease-in-out flex items-center whitespace-nowrap"
-              id="dropdownMenuButton2"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Select pipeline
-              <ChevronDownIcon className="w-5 h-5 ml-2" />
-            </button>
-            <ul className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg
+                id="dropdownMenuButton2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Select pipeline
+                <ChevronDownIcon className="w-5 h-5 ml-2" />
+              </button>
+              <ul className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg
                   shadow-lg mt-1 hidden m-0 bg-clip-padding border-none ">
-              {data.pipelineListItems.map((pipeline) => (
-                <li key={pipeline.id}>
-                  <NavLink
-                    className="
+                {data.pipelineListItems.map((pipeline) => (
+                  <li key={pipeline.id}>
+                    <NavLink
+                      className="
                       flex
                       dropdown-item
                       text-sm
@@ -98,16 +105,16 @@ export default function PipelinesPage() {
                       text-gray-700
                       hover:bg-gray-100
                       "
-                    to={pipeline.id}
-                  >
-                    <PipelineIcon className="w-4 h-4 mt-0.5 mr-2" />
-                    {pipeline.name}
-                  </NavLink>
-                </li>
-              ))}
-              <li>
-                <NavLink
-                  className="
+                      to={pipeline.id}
+                    >
+                      <PipelineIcon className="w-4 h-4 mt-0.5 mr-2" />
+                      {pipeline.name}
+                    </NavLink>
+                  </li>
+                ))}
+                <li>
+                  <NavLink
+                    className="
                     flex
                     dropdown-item
                     text-sm
@@ -121,18 +128,22 @@ export default function PipelinesPage() {
                     text-gray-700
                     hover:bg-gray-100
                   "
-                  to="new"
-                >
-                  <PlusIcon className="w-4 h-4 mt-0.5 mr-2" />
-                  New pipeline
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          {data.pipelineListItems.length == 0 && (
-            <Link to="new" className="flex px-4 py-2.5 text-white font-medium rounded bg-blue-600 text-sm hover:bg-blue-700 hover:shadow-md focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                  active:bg-blue-800 active:shadow-lg active:text-white transition duration-150 ease-in-out">
-              <PlusIcon className="w-4 h-4 mt-0.5 mr-2" /> New Pipeline
+                    to="new"
+                  >
+                    <PlusIcon className="w-4 h-4 mt-0.5 mr-2" />
+                    New pipeline
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          )}
+          {data.pipelineListItems.length === 0 && (
+            <Link
+              to="new"
+              className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <PipelineIcon className="mx-auto h-10 w-10 text-gray-400" />
+              <span className="mt-2 font-medium text-gray-900">Create a new Pipeline</span>
             </Link>
           )}
         </div>
