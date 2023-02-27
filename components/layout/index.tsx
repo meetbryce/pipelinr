08f8f2c47616fcd47e8148beb8f4555901c9c838
Bryce York <bryce@bryceyork.com>
2023-02-26 18:41:31 -0800

    WIP: migrate to next - Layout tidying

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,8 +1,6 @@
 import { useSession } from "next-auth/react";
 import { ReactNode } from "react";
-import useScroll from "@/lib/hooks/use-scroll";
 import Meta from "./meta";
-import { useSignInModal } from "./sign-in-modal";
 import Navigation from "@/components/shared/Navigation";
 
 export default function Layout({
@@ -17,29 +15,25 @@ export default function Layout({
   children: ReactNode;
 }) {
   const { data: session, status } = useSession();
-  const { SignInModal, setShowSignInModal } = useSignInModal();
-  const scrolled = useScroll(50);
-
-  const footerText = <small>AskUnify <span style={{
-    "display": "inline-block",
-    "transform": "scaleX(-1)",
-    "filter": "FlipH"
-  }}>&copy;</span><span>&nbsp;2023</span></small>;
 
   return (
     <>
       <Meta {...meta} />
-      <SignInModal />
       <Navigation />
       <main
-        className="flex w-full h-screen flex-col items-center py-6 bg-gradient-to-br from-indigo-50 via-white">
+        className="flex w-full h-[calc(100vh-68px)] flex-col items-center py-6 bg-gradient-to-br from-indigo-50 via-white">
         {children}
+
+        {/*<div className="w-full border-t border-gray-200 py-5 text-center">
+          <p className="text-gray-500 text-sm">
+            AskUnify <span style={{
+            "display": "inline-block",
+            "transform": "scaleX(-1)",
+            "filter": "FlipH"
+          }}>&copy;</span><span>&nbsp;2023</span>
+          </p>
+        </div>*/}
       </main>
-      <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
-        <p className="text-gray-500">
-          {footerText}
-        </p>
-      </div>
     </>
   );
 }
