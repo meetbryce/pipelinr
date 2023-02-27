bd34f9fd3b143f6910a6aafd2a9ec79a7817ac25
Bryce York <bryce@bryceyork.com>
2023-02-27 08:22:37 -0800

    WIP: migrate to next - Abstracted the 'pipelines' layout & roughed out 'new pipelines' page

---
+++ components/layout/PipelinesLayout.tsx
@@ -0,0 +1,114 @@
+import Link from "next/link";
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  PlusIcon,
+  Square3Stack3DIcon as PipelineIcon
+} from "@heroicons/react/24/outline";
+import { Menu, Transition } from "@headlessui/react";
+import React, { Fragment, ReactNode } from "react";
+import NavLink from "@/components/shared/NavLink";
+import Layout from "@/components/layout/index";
+import { useSession } from "next-auth/react";
+import AuthenticationRequired from "@/components/shared/AuthenticationRequired";
+
+export default function PipelinesLayout({ children }: { children: ReactNode }) {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "unauthenticated") return <AuthenticationRequired />;
+
+  const inactiveClasses = "flex text-gray-700 hover:bg-gray-100";
+  const activeClasses = "flex bg-gray-200 text-gray-900";
+
+  let pipelineListItems: any[] = []; //fixme: fetch & types
+  pipelineListItems = [{ id: 1, name: "Example Pipeline" }, { id: 2, name: "FAKE Pipeline" }]; //fixme: fetch & types
+  // fixme: pass ^^ to <children> to avoid double fetch?
+
+  return (
+    <Layout>
+      <div className="w-full px-6">
+        <nav className="flex px-6 py-4" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-2">
+            <li>
+              <div className="flex items-center">
+                <Link href="."
+                      className="text-sm font-medium text-gray-500 hover:text-blue-600">Pipelines</Link>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <ChevronRightIcon className="h-5 w-5 mr-1 md:mr-2 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                <Menu as="div" className="relative inline-block text-left">
+                  <div>
+                    <Menu.Button
+                      className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                      Select pipeline {/* fixme: show the name of the current pipeline if viewing a pipeline */}
+                      <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items
+                      className="absolute left-0 z-10 mt-2 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-1">
+                        {pipelineListItems.map((pipeline) => (
+                          <NavLink
+                            key={pipeline.id}
+                            href={`/pipelines/${pipeline.id}`}
+                            className={(isActive) => isActive ? activeClasses : inactiveClasses}
+                          >
+                            <Menu.Item as="div" className="flex w-full px-4 py-2 text-sm">
+                              <PipelineIcon className="w-4 h-4 mt-0.5 mr-2" />
+                              {pipeline.name}
+                            </Menu.Item>
+                          </NavLink>
+                        ))}
+                        <NavLink
+                          href="/pipelines/new" exact={true}
+                          className={(isActive) => isActive ? activeClasses : inactiveClasses}
+                        >
+                          <Menu.Item as="div" className="flex w-full px-4 py-2 text-sm">
+                            <PlusIcon className="w-4 h-4 mt-0.5 mr-2" />
+                            New pipeline
+                          </Menu.Item>
+                        </NavLink>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <main className="flex flex-col h-full">
+          {pipelineListItems.length === 0 && (
+            <div className="flex px-6 space-x-1 md:space-x-3">
+              <Link
+                href="new"
+                className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <PipelineIcon className="mx-auto h-10 w-10 text-gray-400" />
+                <span className="mt-2 font-medium text-gray-900">Create a new Pipeline</span>
+              </Link>
+            </div>
+          )}
+
+          <div className="flex-1 px-6 py-4">
+            {children}
+          </div>
+
+        </main>
+      </div>
+    </Layout>
+  );
+}
---
+++ pages/pipelines/new.tsx
@@ -0,0 +1,59 @@
+import PipelinesLayout from "@/components/layout/PipelinesLayout";
+import { Square3Stack3DIcon as PipelineIcon } from "@heroicons/react/24/outline";
+
+
+export default function NewPipelinesPage() {
+  const actionData: { errors?: { name: string } } = { errors: undefined }; //fixme, unmock
+
+  const inputColorClasses = actionData?.errors?.name ?
+    "border-red-300 focus:border-red-500 focus:ring-red-500 focus-visible:outline-red-500 placeholder-red-400" :
+    "border-gray-300 focus:border-blue-500 focus:ring-blue-500";
+
+  return (
+    <PipelinesLayout>
+      <form
+        method="post"
+        className="mx-auto max-w-xl mt-10"
+      >
+        <div>
+          <div className="text-center">
+            <PipelineIcon className="mx-auto h-10 w-10 text-gray-400" />
+            <h2 className="mt-1 mb-2 text-2xl font-medium text-gray-900">Create new pipeline</h2>
+            <p className="mt-1 text-sm text-gray-500">
+              Pipelines allow you to merge, filter, and aggregate data for easy reuse in the future.
+            </p>
+          </div>
+          <div className="mt-6 flex">
+            <label htmlFor="name" className="sr-only">
+              Pipeline name
+            </label>
+            <input
+              className={"block w-full px-3 rounded border shadow-sm sm:text-sm " + inputColorClasses}
+              placeholder="Enter a name for your pipeline"
+              aria-invalid={actionData?.errors?.name ? true : undefined}
+              aria-errormessage={
+                actionData?.errors?.name ? "name-error" : undefined
+              }
+            />
+            <button
+              type="submit"
+              className="ml-4 flex-shrink-0 rounded border border-transparent bg-blue-600 px-4 py-2
+            text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2
+            focus:ring-blue-500 focus:ring-offset-2"
+            >
+              Create Pipeline
+            </button>
+          </div>
+        </div>
+
+        <div>
+          {actionData?.errors?.name && (
+            <div className="mt-2 text-sm text-red-600" id="name-error">
+              {actionData.errors.name[0].toUpperCase() + actionData.errors.name.slice(1) /* quickly uppercase first letter */}
+            </div>
+          )}
+        </div>
+      </form>
+    </PipelinesLayout>
+  );
+}
