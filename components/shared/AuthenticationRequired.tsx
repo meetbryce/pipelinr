5a38bcb787169125bba2c884dc2b17576303aaa0
Bryce York <bryce@bryceyork.com>
2023-02-26 18:29:34 -0800

    WIP: migrate to next - protected route & component

---
+++ components/shared/AuthenticationRequired.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { signIn } from "next-auth/react";
+import Layout from "@/components/layout";
+import React from "react";
+
+export default function AuthenticationRequired() {
+  return <Layout>
+    <div className="grid min-h-full place-items-center py-24 px-6 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-red-600">Access Denied</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Authentication Required</h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">Sorry, you need to be logged in to access this page.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link href="/"
+                className="text-sm font-semibold text-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4">
+            <span aria-hidden="true">&larr;</span>&nbsp;&nbsp;Go back home
+          </Link>
+          <button
+            onClick={() => signIn()}
+            className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            Log in
+          </button>
+        </div>
+      </div>
+    </div>
+  </Layout>;
+}
--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,36 +1,38 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import React from "react";
-import Navigation from "@/components/shared/Navigation";
 import NavLink from "@/components/shared/NavLink";
 import { classNames } from "@/lib/utils";
 import Layout from "@/components/layout";
+import AuthenticationRequired from "@/components/shared/AuthenticationRequired";
 
 export default function SandboxPage() {
-  const { data: session } = useSession();
-  console.log(session);
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "unauthenticated") return <AuthenticationRequired />;
 
   const btnClasses = "rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black";
 
   return (
     <Layout>
-        <div className="w-full px-6">
-          <ul className={'flex md:space-x-3'}>
-            <li><NavLink href={"/"} exact={true} className="text-blue-500 hover:underline">Home</NavLink></li>
-            <li><NavLink href={"/sandbox"} className="text-blue-500 hover:underline">Sandbox</NavLink></li>
-            <li><NavLink href={"/pipelines"} className="text-blue-500 hover:underline">Pipelines</NavLink></li>
-            <li><NavLink href={"/pipelines"}
-                         className={(isActive: boolean) => isActive ? "ACTIVE" : "inactive"}>Pipelines</NavLink></li>
-            <li><NavLink href={"/pipelines"}
-                         className={(isActive: boolean) => classNames(isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")}>Pipelines</NavLink>
-            </li>
-            <li><NavLink href={"/sandbox"}
-                         className={(isActive: boolean) => classNames(isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")}>Sandbox</NavLink>
-            </li>
-          </ul>
-          <p className="my-4">
-            <button className={btnClasses} onClick={() => signIn()}>Sign in</button>
-          </p>
-        </div>
+      <div className="w-full px-6">
+        <ul className={"flex md:space-x-3"}>
+          <li><NavLink href={"/"} exact={true} className="text-blue-500 hover:underline">Home</NavLink></li>
+          <li><NavLink href={"/sandbox"} className="text-blue-500 hover:underline">Sandbox</NavLink></li>
+          <li><NavLink href={"/pipelines"} className="text-blue-500 hover:underline">Pipelines</NavLink></li>
+          <li><NavLink href={"/pipelines"}
+                       className={(isActive: boolean) => isActive ? "ACTIVE" : "inactive"}>Pipelines</NavLink></li>
+          <li><NavLink href={"/pipelines"}
+                       className={(isActive: boolean) => classNames(isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")}>Pipelines</NavLink>
+          </li>
+          <li><NavLink href={"/sandbox"}
+                       className={(isActive: boolean) => classNames(isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")}>Sandbox</NavLink>
+          </li>
+        </ul>
+        <p className="my-4">
+          <button className={btnClasses} onClick={() => signIn()}>Sign in</button>
+        </p>
+      </div>
     </Layout>
   );
 }
